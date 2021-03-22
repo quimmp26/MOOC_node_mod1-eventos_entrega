b85e3d937874a0ec1d732fbac01669cd7de73ceb
quimmp26 <quiim2000@gmail.com>
2021-03-22 19:08:13 +0100

    Crear programador

---
+++ programador.js
@@ -0,0 +1,12 @@
+const EventEmitter = require("events");
+// Clase programador.
+// Programar temperatura segun la hora. (Simula la programación de la temperatura de una habitación a una hora determinada.)
+class Programador extends EventEmitter {
+
+  constructor() {
+    super();
+  }
+
+}
+
+exports = module.exports = Programador;
